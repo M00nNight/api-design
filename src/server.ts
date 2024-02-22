f48b21bc1beea87909d9fbcf4c01cf5e23ad18d7
M00nNight <khanzamil267@gmail.com>
2024-02-22 23:12:18 +0530

    Fix:Convert js to ts sever files

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
-const express = require("express");
-
+import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
@@ -8,4 +7,4 @@ app.get("/", (req, res) => {
   res.json({ massage: "Hello from express!" });
 });
 
-module.exports = app;
+export default app;
